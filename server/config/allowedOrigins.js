1fe00e19e667c1d2b8a4a20bbd1fa5d52e4f54dc
Petro Krevenets <peter.k.nets@gmail.com>
2026-07-07 13:06:26 +0300

    feat(config): add additional allowed origins for local development

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,8 +1,10 @@
 const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:5000",
+  "http://localhost:6000",
   "http://localhost:5005",
   "http://localhost:5000/",
+  "http://localhost:6000/",
   "https://me-freelancer-v1-duml.onrender.com",
   "https://tranquil-woodland-50991.herokuapp.com",
 ];
